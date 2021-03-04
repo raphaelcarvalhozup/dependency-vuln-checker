1680d47174e03325fb5322113c8a7b09a9c696ef
guilhermemarimzup <guilherme.marim@zup.com.br>
2021-03-04 17:01:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const github = require('@actions/github')
 try {
 
     const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}`);
+    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput('time', time);
 
-    const playload = JSON.stringify(github.context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+    
 } catch (error) {
     core.setFailed(error.message);
 }
