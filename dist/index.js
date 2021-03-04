adf7bb27b179bf2ea1269a0f541414d43e7f1290
guilhermemarimzup <guilherme.marim@zup.com.br>
2021-03-04 17:07:43 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -11,12 +11,13 @@ const github = __nccwpck_require__(804)
 try {
 
     const nameToGreet = core.getInput('who-to-greet');
-    console.log('Hello ${nameToGreet}');
+    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput('time', time);
 
-    const playload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log('The event payload: ${payload}');
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
 } catch (error) {
     core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
 
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-    
+
 } catch (error) {
     core.setFailed(error.message);
 }
