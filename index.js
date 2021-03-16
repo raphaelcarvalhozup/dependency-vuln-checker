a099388e9dc2d0540e35a17ed54455d4b8d26427
guilhermemarimzup <guilherme.marim@zup.com.br>
2021-03-16 15:59:45 -0300

    testing Java analysis

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
 const { exec } = require('child_process');
 const artifact = require('@actions/artifact');
 
@@ -19,11 +17,22 @@ try {
         }
 
         if (stdout !== null) {
+
             exec("curl -fsSL https://github.com/jeremylong/DependencyCheck/releases/download/v6.1.2/dependency-check-6.1.2-release.zip -o dependency-check.zip &&\
             unzip -q dependency-check.zip &&\
             ./dependency-check/bin/dependency-check.sh -s pom.xml &&\
-            ls -la")
-            const artifactClient = artifact.create();
+            ls -la" , (error, stdout, stderr) => {
+                
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+
+                const artifactClient = artifact.create();
                 const report = 'dependency-check-report';
                 const rootDirectory = './'
                 const file = ['./dependency-check-report.html'];
@@ -31,10 +40,14 @@ try {
                     continueOnError: true
                 }
                 artifactClient.uploadArtifact(report, file, rootDirectory, options);
+            })
+        
         }
+
     });
 
     exec("find . -name 'package.json' ! -path './node_modules/*'", (error, stdout, stderr) => {
+
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -47,6 +60,7 @@ try {
         if (stdout !== null) {
 
             exec("sudo npm install -g npm-audit-html && npm audit --json | npm-audit-html --output dependency-report.html", (error, stdout, stderr) => {
+                
                 if (error) {
                     console.log(`error: ${error.message}`);
                     return;
