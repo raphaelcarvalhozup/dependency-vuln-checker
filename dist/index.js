b445b2cdb2ea6c83e36e0b51330a015a94b00387
guilhermemarimzup <guilherme.marim@zup.com.br>
2021-03-16 15:51:38 -0300

    testing Java analysis

--- dist/index.js
+++ dist/index.js
@@ -14,25 +14,30 @@ const artifact = __nccwpck_require__(9225);
 try {
 
     exec("find . -name 'pom.xml'", (error, stdout, stderr) => {
+        
         if (error) {
             console.log(`error: ${error.message}`);
             return;
         }
+
         if (stderr) {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        exec("curl -fsSL https://github.com/jeremylong/DependencyCheck/releases/download/v6.1.2/dependency-check-6.1.2-release.zip -o dependency-check.zip &&\
-        unzip -q dependency-check.zip &&\
-        ./dependency-check/bin/dependency-check.sh -s pom.xml")
-        const artifactClient = artifact.create();
-            const report = 'dependency-check-report';
-            const rootDirectory = './'
-            const file = ['./dependency-check-report.html'];
-            const options = {
-                continueOnError: true
-            }
-            artifactClient.uploadArtifact(report, file, rootDirectory, options);
+
+        if (stdout !== null) {
+            exec("curl -fsSL https://github.com/jeremylong/DependencyCheck/releases/download/v6.1.2/dependency-check-6.1.2-release.zip -o dependency-check.zip &&\
+            unzip -q dependency-check.zip &&\
+            ./dependency-check/bin/dependency-check.sh -s pom.xml")
+            const artifactClient = artifact.create();
+                const report = 'dependency-check-report';
+                const rootDirectory = './'
+                const file = ['./dependency-check-report.html'];
+                const options = {
+                    continueOnError: true
+                }
+                artifactClient.uploadArtifact(report, file, rootDirectory, options);
+        }
     });
 
     exec("find . -name 'package.json' ! -path './node_modules/*'", (error, stdout, stderr) => {
@@ -44,25 +49,29 @@ try {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        exec("sudo npm install -g npm-audit-html && npm audit --json | npm-audit-html --output dependency-report.html", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
 
-            const artifactClient = artifact.create();
-            const report = 'dependency-check-report';
-            const rootDirectory = './'
-            const file = ['./dependency-report.html'];
-            const options = {
-                continueOnError: true
-            }
-            artifactClient.uploadArtifact(report, file, rootDirectory, options);
-        })
+        if (stdout !== null) {
+            
+            exec("sudo npm install -g npm-audit-html && npm audit --json | npm-audit-html --output dependency-report.html", (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+
+                const artifactClient = artifact.create();
+                const report = 'dependency-check-report';
+                const rootDirectory = './'
+                const file = ['./dependency-report.html'];
+                const options = {
+                    continueOnError: true
+                }
+                artifactClient.uploadArtifact(report, file, rootDirectory, options);
+            })
+        }
     });
 
 } catch (error) {
--- index.js
+++ index.js
@@ -7,25 +7,30 @@ const artifact = require('@actions/artifact');
 try {
 
     exec("find . -name 'pom.xml'", (error, stdout, stderr) => {
+        
         if (error) {
             console.log(`error: ${error.message}`);
             return;
         }
+
         if (stderr) {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        exec("curl -fsSL https://github.com/jeremylong/DependencyCheck/releases/download/v6.1.2/dependency-check-6.1.2-release.zip -o dependency-check.zip &&\
-        unzip -q dependency-check.zip &&\
-        ./dependency-check/bin/dependency-check.sh -s pom.xml")
-        const artifactClient = artifact.create();
-            const report = 'dependency-check-report';
-            const rootDirectory = './'
-            const file = ['./dependency-check-report.html'];
-            const options = {
-                continueOnError: true
-            }
-            artifactClient.uploadArtifact(report, file, rootDirectory, options);
+
+        if (stdout !== null) {
+            exec("curl -fsSL https://github.com/jeremylong/DependencyCheck/releases/download/v6.1.2/dependency-check-6.1.2-release.zip -o dependency-check.zip &&\
+            unzip -q dependency-check.zip &&\
+            ./dependency-check/bin/dependency-check.sh -s pom.xml")
+            const artifactClient = artifact.create();
+                const report = 'dependency-check-report';
+                const rootDirectory = './'
+                const file = ['./dependency-check-report.html'];
+                const options = {
+                    continueOnError: true
+                }
+                artifactClient.uploadArtifact(report, file, rootDirectory, options);
+        }
     });
 
     exec("find . -name 'package.json' ! -path './node_modules/*'", (error, stdout, stderr) => {
@@ -37,25 +42,29 @@ try {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        exec("sudo npm install -g npm-audit-html && npm audit --json | npm-audit-html --output dependency-report.html", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
 
-            const artifactClient = artifact.create();
-            const report = 'dependency-check-report';
-            const rootDirectory = './'
-            const file = ['./dependency-report.html'];
-            const options = {
-                continueOnError: true
-            }
-            artifactClient.uploadArtifact(report, file, rootDirectory, options);
-        })
+        if (stdout !== null) {
+            
+            exec("sudo npm install -g npm-audit-html && npm audit --json | npm-audit-html --output dependency-report.html", (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+
+                const artifactClient = artifact.create();
+                const report = 'dependency-check-report';
+                const rootDirectory = './'
+                const file = ['./dependency-report.html'];
+                const options = {
+                    continueOnError: true
+                }
+                artifactClient.uploadArtifact(report, file, rootDirectory, options);
+            })
+        }
     });
 
 } catch (error) {
